9ad934f2444c9cb916e282649eda259108dbdb16
Nisadu Nimsitha <nimsitha2003@gmail.com>
2025-11-02 14:53:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+}
+
+let bell = document.querySelector('.notification');
+
+document.querySelector('#bell-icon').onclick = () => {
+    bell.classList.toggle('active');
+}
+
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
